5244b77a1198432c34c8345a6c320eafbbad76de
Rubén Peregrina <peregrinaruben@gmail.com>
2024-02-02 11:51:31 +0100

    Install Nebular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  styles: [
+    `
+      ::ng-deep nb-layout-column {
+        justify-content: center;
+        display: flex;
+      }
+      nb-chat {
+        width: 500px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'gemini-chat';
+  messages: any[] = [
+    {
+      text: 'Custom template was provided as a title!',
+      date: new Date(),
+      reply: false,
+      user: {
+        name: 'Bot',
+        avatar: 'https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/robot-face.png',
+      },
+    },
+  ];
+
+  sendMessage(e: any) {
+    console.log('sendMessage');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NbChatModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbChatModule,
+    NbLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
